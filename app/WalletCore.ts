0a9b456a5c4f9432615dbca083eb62d1d23aa444
mindsgn <zookid@protonmail.com>
2026-02-27 15:56:08 +0200

    update

--- app/WalletCore.ts
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { WalletCore } = NativeModules;
-
-interface WalletCoreInterface {
-  helloWorld(): Promise<string>;
-}
-
-export default WalletCore as WalletCoreInterface;
